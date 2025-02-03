5287a6be539bd0a61e5f94b31715a638ac9b2c29
JSF <Julian-2119@users.noreply.github.com>
2025-02-03 13:42:32 +0100

    edit promt and existig code

--- main.js
+++ main.js
@@ -7,25 +7,26 @@
 */
 
 import promtSync from 'prompt-sync';
-const prompt = promtSync();
+const Prompt = promtSync()
+
+function prompt(output){
+    let input = Prompt(output);
+    if(input == "EXIT"){
+        console.log("Goodbye");
+        process.exit();
+    }
+    return input
+}
 
 function getMode(data) {
     console.clear();
     console.log("Welcome to the Learning Platform!");
     console.log("Please select a mode:");
 
-    if (data.length < 10) {
-        for (let i = 0; i < data.length; i++) {
-            console.log(` ${i + 1}. ${data[i].name}`);
-        }
-    } else {
-        for (let i = 0; i < 9; i++) {
-            console.log(` ${i + 1}. ${data[i].name}`);
-        }
-        for (let i = 9; i < data.length; i++) {
-            console.log(`${i + 1}. ${data[i].name}`);
-        }
+    for (let i = 0; i < data.length; i++) {
+        console.log(` ${i + 1}. ${data[i].name}`);
     }
+    
     return parseInt(prompt(" > ")) - 1;
 }
 
@@ -39,18 +40,10 @@ function getTopic(data) {
     console.log("Please select a topic:");
     console.log(" 0. Go back");
 
-    if (data.length < 10) {
-        for (let i = 0; i < data.length; i++) {
-            console.log(` ${i + 1}. ${data[i].name}`);
-        }
-    } else {
-        for (let i = 0; i < 9; i++) {
-            console.log(` ${i + 1}. ${data[i].name}`);
-        }
-        for (let i = 9; i < data.length; i++) {
-            console.log(`${i + 1}. ${data[i].name}`);
-        }
+    for (let i = 0; i < data.length; i++) {
+        console.log(` ${i + 1}. ${data[i].name}`);
     }
+
     return parseInt(prompt(" > ")) - 1;
 }
 
@@ -58,12 +51,13 @@ function getTopic(data) {
  * TODO: Read Mode-Data to list
 **/
 
-let modeData = [];
+let modeData = [
+    { name:"edit Category" }];
 let topicData = [];
 let topicIdx = -1;
 
 while (topicIdx === -1) {
-    modeIdx = getMode(modeData);
+    let modeIdx = getMode(modeData);
     topicData = getAvailableTopics(modeData[modeIdx]);
     topicIdx = getTopic(topicData);
 }
