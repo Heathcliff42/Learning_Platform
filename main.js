34042ef867e3d2df4cf97c96d9090c590a5cafda
Lukas Kroczek <194911305+Heathcliff42@users.noreply.github.com>
2025-02-03 15:02:12 +0100

    Refactor main.js: improved display of mode and topic choices, updated TODO comments

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
     * @Author: Lukas Kroczek
     * @Date: 2025-02-03
     * @Description: Learning Platform
-    * @Version: 1.0
+    * @Version: 1.0.0
     * @LastUpdate: 2025-02-03
 */
 
 import promtSync from 'prompt-sync';
-const Prompt = promtSync()
+const PROMPT = promtSync()
 
 function prompt(output){
-    let input = Prompt(output);
-    if(input == "EXIT"){
+    let input = PROMPT(output);
+    if(input === "EXIT"){
         console.log("Goodbye");
         process.exit();
     }
@@ -23,8 +23,17 @@ function getMode(data) {
     console.log("Welcome to the Learning Platform!");
     console.log("Please select a mode:");
 
-    for (let i = 0; i < data.length; i++) {
-        console.log(` ${i + 1}. ${data[i].name}`);
+    if (data.length < 9) {
+        for (let i = 0; i < data.length; i++) {
+            console.log(` ${i + 1}. ${data[i]}`);
+        }
+    } else {
+        for (let i = 0; i < 9; i++) {
+            console.log(` ${i + 1}. ${data[i]}`);
+        }
+        for (let i = 9; i < data.length; i++) {
+            console.log(`${i + 1}. ${data[i]}`);
+        }
     }
     
     return parseInt(prompt(" > ")) - 1;
@@ -32,27 +41,39 @@ function getMode(data) {
 
 function getAvailableTopics(mode){
     /*
-     * TODO: Read available Topics to list
+     * TODO: Read available topics from database to list
+     * (join tables to get topics for the selected mode)
+     * input: mode
+     * return: topicData
     **/
 }
 
 function getTopic(data) {
     console.log("Please select a topic:");
-    console.log(" 0. Go back");
+    console.log(" 0. Choose another mode");
 
-    for (let i = 0; i < data.length; i++) {
-        console.log(` ${i + 1}. ${data[i].name}`);
+    if (data.length < 9) {
+        for (let i = 0; i < data.length; i++) {
+            console.log(` ${i + 1}. ${data[i]}`);
+        }
+    } else {
+        for (let i = 0; i < 9; i++) {
+            console.log(` ${i + 1}. ${data[i]}`);
+        }
+        for (let i = 9; i < data.length; i++) {
+            console.log(`${i + 1}. ${data[i]}`);
+        }
     }
 
     return parseInt(prompt(" > ")) - 1;
 }
 
 /*
- * TODO: Read Mode-Data to list
+ * TODO: Read mode-data from database to list
+ * return: modeData
 **/
 
-let modeData = [
-    { name:"edit Category" }];
+let modeData = [];
 let topicData = [];
 let topicIdx = -1;
 
